a3ed3956051b1fd149064a2b3704d559ed7fee0a
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-03-23 02:32:44 +0000

    Attempt one at open telemetry

--- src/telemetry/tracing.ts
+++ src/telemetry/tracing.ts
@@ -1,3 +1,4 @@
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import {
   BatchSpanProcessor,
   ConsoleSpanExporter,
@@ -6,10 +7,11 @@ import { Resource } from '@opentelemetry/resources';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 import { NodeTracerProvider } from '@opentelemetry/sdk-trace-node';
 import { registerInstrumentations } from '@opentelemetry/instrumentation';
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 
 // Optionally register instrumentation libraries
 registerInstrumentations({
-  instrumentations: [],
+  instrumentations: [getNodeAutoInstrumentations()],
 });
 
 const resource = Resource.default().merge(
@@ -22,7 +24,13 @@ const resource = Resource.default().merge(
 const provider = new NodeTracerProvider({
   resource: resource,
 });
-const exporter = new ConsoleSpanExporter();
+
+const exporter = new OTLPTraceExporter({
+  url: 'http://localhost:4318/v1/traces',
+});
+
+//const exporter = new ConsoleSpanExporter();
+
 const processor = new BatchSpanProcessor(exporter);
 provider.addSpanProcessor(processor);
 
