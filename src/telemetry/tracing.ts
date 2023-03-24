9914932db8240fdb4beb715d2f18eb1c7b6d3b8d
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-03-24 04:51:29 +0000

    Remove sample metrics

--- src/telemetry/tracing.ts
+++ src/telemetry/tracing.ts
@@ -63,25 +63,3 @@ meterProvider.addMetricReader(
     exportIntervalMillis: 1000,
   }),
 );
-
-const meter = meterProvider.getMeter('todo-list-collector');
-
-const requestCounter = meter.createCounter('request_counter', {
-  description: 'Request Counter',
-});
-
-const upDownCounter = meter.createUpDownCounter('up_down_counter', {
-  description: 'Up/Down Counter',
-});
-
-const histogram = meter.createHistogram('histogram', {
-  description: 'Histogram',
-});
-
-const attributes = { pid: process.pid, environment: 'development' };
-
-setInterval(() => {
-  requestCounter.add(1, attributes);
-  upDownCounter.add(Math.random() > 0.5 ? 1 : -1, attributes);
-  histogram.record(Math.random(), attributes);
-}, 1000);
