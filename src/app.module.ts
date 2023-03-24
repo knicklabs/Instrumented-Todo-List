d90b4c05c12ae3517e7d5b46c19f807dd5acc77a
Nickolas Kenyeres <nickolas@knicklabs.com>
2023-03-24 05:27:18 +0000

    Got prometheus working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { PrometheusModule } from '@willsoto/nestjs-prometheus';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TasksModule } from './tasks/tasks.module';
 
 @Module({
   imports: [
+    PrometheusModule.register(),
     TasksModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
